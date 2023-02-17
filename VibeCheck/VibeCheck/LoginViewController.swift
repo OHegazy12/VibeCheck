abe1940a71c6397899a6bf87f62e81d687bd393c
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2023-02-16 21:29:23 -0500

    Add files via upload

---
+++ VibeCheck/VibeCheck/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VibeCheck
+//
+//  Created by Omar Hegazy on 2/16/23.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
