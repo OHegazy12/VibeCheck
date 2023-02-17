f500d4ef2381c8d90c081f4b705aef393f21116b
Omar Hegazy <omarhegazy78@gmail.com>
2023-02-16 21:37:49 -0500

    Commit

--- VibeCheck/LoginViewController.swift
+++ VibeCheck/LoginViewController.swift
@@ -13,8 +13,8 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-    
     
 }
 
